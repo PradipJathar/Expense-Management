3b64293bbe64f0d03f47dcaad5fad1fe8ae8f6b6
PradipJathar <jatharpradipofficial@gmail.com>
2024-02-27 15:40:05 +0530

    Added data annotations to Expenses model.

--- ExpenseManagement/Models/Expenses.cs
+++ ExpenseManagement/Models/Expenses.cs
@@ -8,13 +8,21 @@ namespace ExpenseManagement.Models
         [Key]
         public int ExpenseId { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "Please select expense date.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Expense Date")]
         public DateTime ExpenseDate { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "Please enter expense given to details.")]
+        [MinLength(3, ErrorMessage = "The name of expense given to is too short.")]
+        [Display(Name = "Paid To")]
         public string ExpenseGivenTo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter expense amount.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Please enter valid expense amount." )]
+        [Display(Name = "Expense Amount")]
         public double ExpenseAmount { get; set; }         
     }
 }
