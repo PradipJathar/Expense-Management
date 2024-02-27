7cea57c8c2b666f6a50a6afe395b6956c4b8e51c
PradipJathar <jatharpradipofficial@gmail.com>
2024-02-27 12:44:41 +0530

    Created expense controller and index action.

---
+++ ExpenseManagement/Controllers/ExpenseController.cs
@@ -0,0 +1,23 @@
+﻿using ExpenseManagement.DataLayer;
+using ExpenseManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManagement.Controllers
+{
+	public class ExpenseController : Controller
+	{
+		public readonly ExpenseManagementDBContext db;
+
+        public ExpenseController(ExpenseManagementDBContext context)
+        {
+			db = context;
+        }
+
+        public IActionResult Index()
+		{
+			IEnumerable<Expenses> expensesList = db.Expenses.ToList();
+
+			return View(expensesList);
+		}
+	}
+}
