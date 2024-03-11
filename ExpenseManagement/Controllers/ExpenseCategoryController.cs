6f8af2ef5ec804e0ea7390d76526ae038bfa8728
PradipJathar <jatharpradipofficial@gmail.com>
2024-03-11 17:11:03 +0530

    Added Create, Update, and Delete views for Expense Category.

--- ExpenseManagement/Controllers/ExpenseCategoryController.cs
+++ ExpenseManagement/Controllers/ExpenseCategoryController.cs
@@ -14,11 +14,82 @@ namespace ExpenseManagement.Controllers
 			db = context;
 		}
 
+
 		public IActionResult Index()
 		{
 			IEnumerable<ExpenseCategory> expenseCategories = db.ExpenseCategory.ToList();
 
 			return View(expenseCategories);
 		}
-	}
+
+
+        public IActionResult Create(ExpenseCategory expenseCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ExpenseCategory.Add(expenseCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        public IActionResult GetExpenseCategoryDetailsForUpdate(int? id)
+        {
+            var expenseCategory = db.ExpenseCategory.Find(id);
+
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenseCategory);
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(ExpenseCategory expenseCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ExpenseCategory.Update(expenseCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        public IActionResult GetExpenseCategoryDetailsForDelete(int? id)
+        {
+            var expenseCategory = db.ExpenseCategory.Find(id);
+
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenseCategory);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int? ExpenseCategoryId)
+        {
+            var expenseCategory = db.ExpenseCategory.Find(ExpenseCategoryId);
+
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+
+            db.ExpenseCategory.Remove(expenseCategory);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
 }
