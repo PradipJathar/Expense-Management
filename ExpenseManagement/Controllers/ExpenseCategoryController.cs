0b3066787ece33cd42cd95c1ea756862073c86b6
PradipJathar <jatharpradipofficial@gmail.com>
2024-03-11 15:57:34 +0530

    Created ExpenseCategoryController and added db context in it.

---
+++ ExpenseManagement/Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseManagement.DataLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManagement.Controllers
+{
+	public class ExpenseCategoryController : Controller
+	{
+		private readonly ExpenseManagementDBContext db;
+
+		public ExpenseCategoryController(ExpenseManagementDBContext context)
+		{
+			db = context;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
